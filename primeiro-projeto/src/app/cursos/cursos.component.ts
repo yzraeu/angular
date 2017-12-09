2a41e418376d94ab8bd32cda501582dc8667d621
Israel Rodriguez <yzraeu@gmail.com>
2017-12-09 02:19:18 -0200

    template class

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['.NET', 'Angular', 'MongoDB'];
+
+  constructor() {
+    this.nomePortal = 'http://angular.io';
+  }
 
   ngOnInit() {
   }
