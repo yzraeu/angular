3bb9a31c1528b71784e32e4cabae969c1db3db95
Israel Rodriguez <yzraeu@gmail.com>
2017-12-09 02:37:00 -0200

    services + di class

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['.NET', 'Angular', 'MongoDB'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://angular.io';
+
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CursosService } from './cursos.service';
+
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [CursosComponent],
-  declarations: [CursosComponent, CursoDetalheComponent]
+  declarations: [CursosComponent, CursoDetalheComponent],
+  providers: [CursosService]
 })
 export class CursosModule { }
