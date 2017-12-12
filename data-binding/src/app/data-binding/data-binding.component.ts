b7300b52deae732de068f0d7ebfd4c00ce76753c
Israel Rodriguez <yzraeu@gmail.com>
2017-12-12 13:33:24 -0200

    event binding class

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,12 @@ export class DataBindingComponent implements OnInit {
   url = 'http://loiane.com';
   cursoAngular = true;
   urlImagem = 'http://lorempixel.com/400/200/sports';
+  buttonMessage: string = 'Me clique!';
+  contagem: number = 0;
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  mousePorCima: boolean = false;
+  textoMouse: string = 'Passe sobre mim';
 
   constructor() { }
 
@@ -23,5 +29,23 @@ export class DataBindingComponent implements OnInit {
   getCurtirCurso() {
     return true;
   }
+
+  botaoClicado(){
+    this.contagem++;
+    this.buttonMessage = 'Obrigado ' + this.contagem;
+  }
+
+  teclaPressionada (evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salva(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  mouseOverAndOut(){
+    this.mousePorCima = !this.mousePorCima;
+    this.textoMouse = this.mousePorCima ? 'Aaee' : 'Passe sobre mim';
+  } 
 
 }
