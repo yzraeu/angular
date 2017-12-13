bb20a88f0f4ccf78b504badd13884576be560b7c
Israel Rodriguez <yzraeu@gmail.com>
2017-12-13 16:07:05 -0200

    ngIf class

---
+++ diretivas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
