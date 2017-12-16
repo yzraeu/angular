efcb8d674d006b3c50d7b4a4a3ec7b56039005db
Israel Rodriguez <yzraeu@gmail.com>
2017-12-16 03:02:28 -0200

    elvis operator class

---
+++ diretivas/src/app/operador-elvis/operador-elvis.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operador-elvis',
+  templateUrl: './operador-elvis.component.html',
+  styleUrls: ['./operador-elvis.component.css']
+})
+export class OperadorElvisComponent implements OnInit {
+
+  tarefa: any = {
+    descricao: 'Descrição',
+    responsavel: null
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
