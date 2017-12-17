f317385a50fc42bdc53a6299d796e03b7e30864d
Israel Rodriguez <yzraeu@gmail.com>
2017-12-17 20:19:45 -0200

    change pipe LOCALE_ID

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
+  imports: [BrowserModule],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: settingsService => settingsService.getLocale()
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  // Modo alternativo
+  // providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ pipes/src/app/settings.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SettingsService {
+  constructor() {}
+
+  getLocale() {
+    return 'pt-BR';
+  }
+}
