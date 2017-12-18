fb9c2f08517680018da49b84267d88e5930e8c19
Israel Rodriguez <yzraeu@gmail.com>
2017-12-18 12:03:24 -0200

    routes project  init

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
