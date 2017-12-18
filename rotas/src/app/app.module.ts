bc5acf5c918348549b5fa72eb6c8f89aab00b8d6
Israel Rodriguez <yzraeu@gmail.com>
2017-12-18 12:31:18 -0200

    router config

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    NoopAnimationsModule,
+    routing
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const APP_ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'cursos', component: CursosComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+
+/*import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'path', component: FeatureComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+*/
