444e48b7f81fdf1807ea3884deb94b4c977378c8
Israel Rodriguez <yzraeu@gmail.com>
2017-12-18 15:54:19 -0200

    queryparam + fake pagination

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -8,11 +11,34 @@ import { CursosService } from './cursos.service';
 })
 export class CursosComponent implements OnInit {
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private _cursosService: CursosService) {}
+  constructor(
+    private _cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.cursos = this._cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      q => (this.pagina = q.pagina)
+    );
   }
 
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    this.pagina++;
+    this.router.navigate(['/cursos'], { queryParams: { pagina: this.pagina } });
+  }
+
+  paginaAnterior() {
+    this.pagina--;
+    this.router.navigate(['/cursos'], { queryParams: { pagina: this.pagina } });
+  }
 }
