2968a0b57fc96a1aab299f303e4defa9e7d28308
Israel Rodriguez <yzraeu@gmail.com>
2017-12-30 18:32:10 -0200

    cep get request

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -23,7 +23,7 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.usuario);
   }
 
-  consultaCEP(cep) {
+  consultaCEP(cep, formulario) {
     cep = cep.replace(/\D/g, '');
 
     if (cep === '') {
@@ -36,12 +36,52 @@ export class TemplateFormComponent implements OnInit {
       return;
     }
 
+    this.limpaForm(formulario);
+
     this.http
       .get(`//viacep.com.br/ws/${cep}/json/`)
       .map(dados => dados.json())
-      .subscribe(dados => {
-        console.log(dados);
-      });
+      .subscribe(dados => this.populaDados(dados, formulario));
+  }
+
+  populaDados(dados, form: NgForm) {
+    // form.setValue({
+    //   nome: form.value.nome,
+    //   email: form.value.email,
+    //   endereco: {
+    //     rua: dados.logradouro,
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // });
+
+    form.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  limpaForm(form) {
+    form.form.patchValue({
+      endereco: {
+        rua: '..',
+        cep: '..',
+        complemento: '..',
+        bairro: '..',
+        cidade: '..',
+        estado: '..'
+      }
+    });
   }
 
   campoValido(campo) {
